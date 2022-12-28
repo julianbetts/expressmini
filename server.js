a41e1230ab84dd579655b631113d7071d296d1c6
Julian Betts <wjulianbetts@gmail.com>
2022-12-27 17:13:22 -0800

    added student mini project

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+// Import custom middleware, "cLog"
+app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+);
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+app.get('*', (req, res) => //* means anything typed in url that is not an established path
+res.sendFile(path.resolve(__dirname, './public/404.html')) //path.resolve method resolves a method of paths to one absolute path
+)
